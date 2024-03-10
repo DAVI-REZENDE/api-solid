b60dc2d54031b0ae5838dd421465381680e348d4
Davi Rezende <daviresendes12@gmail.com>
2024-03-09 23:00:25 -0300

    feat: implement use cases, repositories and controllers with unitary tests

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -1,5 +1,5 @@
-import { prisma } from '@/lib/prisma'
-import { hash } from 'bcryptjs'
+import { UserAlreadyExistsError } from '@/use-cases/erros/user-already-exists-error'
+import { makeRegisterUseCase } from '@/use-cases/factories/make-register-use-case'
 import { FastifyReply, FastifyRequest } from 'fastify'
 import { z } from 'zod'
 
@@ -12,25 +12,17 @@ export async function register(request: FastifyRequest, reply: FastifyReply) {
 
   const { name, email, password } = registerBodySchema.parse(request.body)
 
-  const password_hash = await hash(password, 6)
+  try {
+    const registerUseCase = makeRegisterUseCase()
 
-  const userWithSameEmail = await prisma.user.findUnique({
-    where: {
-      email,
-    },
-  })
+    registerUseCase.execute({ name, email, password })
+  } catch (error) {
+    if (error instanceof UserAlreadyExistsError) {
+      return reply.status(409).send({ message: error.message })
+    }
 
-  if (userWithSameEmail) {
-    return reply.status(409).send()
+    throw error
   }
-
-  await prisma.user.create({
-    data: {
-      name,
-      email,
-      password_hash,
-    },
-  })
 
   return reply.status(201).send()
 }
